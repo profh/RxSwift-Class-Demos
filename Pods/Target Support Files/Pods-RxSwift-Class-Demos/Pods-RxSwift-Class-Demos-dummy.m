19f94dd4ab3caf2f529d5ec7713508b51b12f01a
profh <profh@cmu.edu>
2018-10-22 13:59:57 -0400

    Revised Subjects playground to remove whatever bug was causing Xcode to crash

---
+++ Pods/Target Support Files/Pods-RxSwift-Class-Demos/Pods-RxSwift-Class-Demos-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwift_Class_Demos : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwift_Class_Demos
+@end
---
+++ RxSwift-Class-Demos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwift-Class-Demos
+//
+//  Created by Larry Heimann on 10/15/18.
+//  Copyright © 2018 Larry Heimann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
